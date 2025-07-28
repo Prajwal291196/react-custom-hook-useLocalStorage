1684ae4f5341e15ac0066f96e0bc1a289022f9d2
Prajwal <prajwalnaik159@gmail.com>
2025-07-28 22:52:48 +0530

    v1.0.0

--- src/todo-list.jsx
+++ src/todo-list.jsx
@@ -1,6 +1,20 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import useLocalStorage from "./use-local-storage";
+import {
+  Container,
+  Typography,
+  Box,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Checkbox,
+  IconButton,
+  Paper
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
@@ -23,31 +37,68 @@ const TodoList = () => {
       })
     );
   };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
   return (
-    <div>
-      <h1>Todo List App</h1>
-      <form onSubmit={handleAddNewTodo}>
-        <input
-          type="text"
-          placeholder="Add a new todo"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-        />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <input
-              type="checkbox"
-              checked={todo.done}
-              onChange={() => handleToggleTodo(todo)}
-            />
-            {todo.text}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Container maxWidth="sm">
+      <Paper elevation={3} sx={{ p: 3, mt: 5 }}>
+        <Typography variant="h4" align="center" gutterBottom>
+          ✅ Todo List
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleAddNewTodo}
+          sx={{ display: "flex", gap: 2, mb: 3 }}
+        ><TextField
+            label="Add a new todo"
+            variant="outlined"
+            fullWidth
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <Button variant="contained" color="primary" type="submit">
+            Add
+          </Button>
+        </Box>
+        {/* Todo List */}
+        <List>
+          {todos.map((todo) => (
+            <ListItem
+              key={todo.id}
+              secondaryAction={
+                <IconButton
+                  edge="end"
+                  aria-label="delete"
+                  onClick={() => handleDeleteTodo(todo.id)}
+                >
+                  <DeleteIcon />
+                </IconButton>
+              }
+              sx={{
+                bgcolor: "background.paper",
+                mb: 1,
+                borderRadius: 2,
+                boxShadow: 1,
+              }}
+            >
+              <Checkbox
+                checked={todo.done}
+                onChange={() => handleToggleTodo(todo)}
+              />
+              <ListItemText
+                primary={todo.text}
+                sx={{
+                  textDecoration: todo.done ? "line-through" : "none",
+                  color: todo.done ? "gray" : "inherit",
+                }}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+    </Container>
   );
 };
 
